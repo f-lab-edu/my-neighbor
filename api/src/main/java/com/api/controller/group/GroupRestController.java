2d9e8a8fabf855787a3659b8a7276c6fd999f9a2
이유나 <yuna0560@naver.com>
2022-12-22 18:34:02 +0900

    - GroupRestController return value 추가 - 루트 프로젝트 group id 변경 (example -> flab)

--- api/src/main/java/com/api/controller/group/GroupRestController.java
+++ api/src/main/java/com/api/controller/group/GroupRestController.java
@@ -1,45 +1,129 @@
 package com.api.controller.group;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/group")
 public class GroupRestController {
 
-    //그룹 생성
-    @PostMapping
-    public Object group() {
-    	return null;
+    // 그룹 조회
+    @GetMapping
+    public Object getGroups() {
+        return "{\n" +
+                "   \"success\" : true, \n" +
+                "   \"response\": {\n" +
+                "       \"groupList\":[\n" +
+                "           {\n" +
+                "               \"groupId\": 2401, \n" +
+                "               \"categoryId\": 103, \n" +
+                "               \"cityId\": 1, \n" +
+                "               \"townId\": 8, \n" +
+                "               \"maxNum\": 150, \n" +
+                "               \"name\": \"서울특별시 서대문구 산악회\", \n" +
+                "               \"desc\": \"서대문구 등산러 모여라\", \n" +
+                "               \"profileImageUrl\": \"https://s3.console.aws.amazon.com/......jpeg\", \n" +
+                "               \"masterId\": 83212, \n" +
+                "               \"modifyAt\": \"2022-12-15\", \n" +
+                "               \"createAt\": \"2022-12-15\", \n" +
+                "           }, \n" +
+                "           {\n" +
+                "               \"groupId\": 5, \n" +
+                "               \"categoryId\": 10, \n" +
+                "               \"cityId\": 4, \n" +
+                "               \"townId\": 2, \n" +
+                "               \"maxNum\": 20, \n" +
+                "               \"name\": \"강원도 춘천시 닭갈비 탐험대\", \n" +
+                "               \"desc\": \"닭갈비를 사랑하는 사람들\", \n" +
+                "               \"profileImageUrl\": \"https://s3.console.aws.amazon.com/......jpeg\", \n" +
+                "               \"masterId\": 673, \n" +
+                "               \"modifyAt\": \"2022-05-10\", \n" +
+                "               \"createAt\": \"2002-12-12\", \n" +
+                "           }, \n" +
+                "       ], \n" +
+                "   \"error\" : null \n" +
+                "}";
     }
 
-    //그룹 조회
-    @GetMapping("/search")
-    public Object groupSearch() {
-    	return null;
-    }
-
-    //특정 그룹 조회
+    // 특정 그룹 조회
     @GetMapping("/{groupId}")
-    public Object groupIdSearch() {
-    	return null;
+    public Object getOneGroup(@PathVariable Long groupId) {
+        return "{\n" +
+                "   \"success\" : true, \n" +
+                "   \"response\": {\n" +
+                "       \"group\" : {\n" +
+                "           \"groupId\": " + groupId + ", \n" +
+                "           \"categoryId\": 103, \n" +
+                "           \"cityId\": 1, \n" +
+                "           \"townId\": 7, \n" +
+                "           \"maxNum\": 15, \n" +
+                "           \"name\": \"서울특별시 동대문구 방탈출 모임\", \n" +
+                "           \"desc\": \"방을 탈출하자\", \n" +
+                "           \"profileImageUrl\": \"https://s3.console.aws.amazon.com/......jpeg\", \n" +
+                "           \"masterId\": 0025345, \n" +
+                "           \"modifyAt\": \"2022-03-20\", \n" +
+                "           \"createAt\": \"2021-02-15\", \n" +
+                "           }, \n" +
+                "       }, \n" +
+                "   \"error\" : null \n" +
+                "}";
     }
 
-    //그룹 수정
+    // 그룹 생성
+    @PostMapping("/create")
+    public Object createGroup(GroupDto groupDto) {
+        return "{\n" +
+                "   \"success\" : true, \n" +
+                "   \"response\": {\n" +
+                "       \"group\" : {\n" +
+                "           \"groupId\": 0002401, \n" +
+                "           \"categoryId\": 103, \n" +
+                "           \"cityId\": 1, \n" +
+                "           \"townId\": 8, \n" +
+                "           \"maxNum\": 10, \n" +
+                "           \"name\": \"서울특별시 서대문구 산악회\", \n" +
+                "           \"desc\": \"서대문구 등산러 모여라\", \n" +
+                "           \"profileImageUrl\": \"https://s3.console.aws.amazon.com/......jpeg\", \n" +
+                "           \"masterId\": 0083212, \n" +
+                "           \"modifyAt\": \"2022-12-22\", \n" +
+                "           \"createAt\": \"2022-12-22\", \n" +
+                "           }, \n" +
+                "       }, \n" +
+                "   \"error\" : null \n" +
+                "}";
+    }
+
+    // 그룹 수정
     @PutMapping("/{groupId}/update")
-    public Object groupUpdate() {
-    	return null;
+    public Object updateGroup(@PathVariable Long groupId) {
+        return "{\n" +
+                "   \"success\" : true, \n" +
+                "   \"response\": {\n" +
+                "       \"group\" : {\n" +
+                "           \"groupId\": 0002401, \n" +
+                "           \"categoryId\": 103, \n" +
+                "           \"cityId\": 1, \n" +
+                "           \"townId\": 8, \n" +
+                "           \"maxNum\": 10, \n" +
+                "           \"name\": \"서울특별시 서대문구 등산 모임\", \n" +
+                "           \"desc\": \"등산러 모이세요\", \n" +
+                "           \"profileImageUrl\": \"https://s3.console.aws.amazon.com/......jpeg\", \n" +
+                "           \"masterId\": 0083212, \n" +
+                "           \"modifyAt\": \"2022-12-22\", \n" +
+                "           \"createAt\": \"2022-12-15\", \n" +
+                "           }, \n" +
+                "       }, \n" +
+                "   \"error\" : null \n" +
+                "}";
     }
 
-    //그룹 삭제
+    // 그룹 삭제
     @DeleteMapping("/{groupId}/delete")
-    public Object groupDelete() {
-    	return null;
+    public Object deleteGroup(@PathVariable Long groupId) {
+        return "{\n" +
+                "   \"success\" : true, \n" +
+                "   \"response\": \"" + groupId + " has been deleted.\", \n" +
+                "   \"error\" : null \n" +
+                "}";
     }
 
     //그룹 포스팅 작성
