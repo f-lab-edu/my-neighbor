831ce9a0de47ae712109b7ed792de236e78b9e5e
김진주 <yrgell@gmail.com>
2023-01-01 15:46:28 +0900

    update conflict

---
+++ api/src/main/java/com/api/repository/group/GroupRepository.java
@@ -0,0 +1,26 @@
+package com.api.repository.group;
+
+import com.api.model.group.group;
+
+import java.util.Optional;
+
+public interface GroupRepository {
+
+
+    // search group
+
+    // create group
+
+    // update group
+
+    // delete group
+
+    // search post
+
+    // write post
+
+    // update post
+
+    // delete post
+
+}
