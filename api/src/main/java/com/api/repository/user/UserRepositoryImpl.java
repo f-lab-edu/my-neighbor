24f59556486a92ceb0580a2abfcb87e7a568866c
이유나 <yuna0560@naver.com>
2023-01-01 15:09:19 +0900

    Repository 어노테이션 추가

--- api/src/main/java/com/api/repository/user/UserRepositoryImpl.java
+++ api/src/main/java/com/api/repository/user/UserRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.api.repository.user;
 
 import com.api.model.user.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public class UserRepositoryImpl implements UserRepository {
+
     @Override
     public User insert(User user) {
         return null;
