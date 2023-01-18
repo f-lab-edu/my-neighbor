87a733058ae22fd4019c3c7c13ee820ec6134148
eyuna <yuna0560@naver.com>
2023-01-18 13:05:10 +0900

    Feat/add jpa (#21)
    * JPA, H2 추가
    * 설정 파일 분리

--- api/src/main/java/com/api/model/user/User.java
+++ api/src/main/java/com/api/model/user/User.java
@@ -1,24 +1,36 @@
 package com.api.model.user;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.StringJoiner;
 
-@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"userId"})
+@Getter
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @NotNull
     private String email;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String password;
 
     private Long cityId;
@@ -33,6 +45,21 @@ public class User {
 
     public User(String email, String name, String password) {
         this(null, email, name, password, null, null, null, null, null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return userId != null ? userId.hashCode() : 0;
     }
 
     @Override
---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(email,name,password) VALUES ('test00@gmail.com','tester00','pw-00');
+INSERT INTO users(email,name,password) VALUES ('test01@gmail.com','tester01','pw-01');
+INSERT INTO users(email,name,password) VALUES ('test02@gmail.com','tester02','pw-02');
