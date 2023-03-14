c54bcbe6a3272bbb0ea0b50be12a5cdf2d60c58a
이유나 <yuna0560@naver.com>
2023-03-14 19:20:23 +0900

    설정파일 루트 모듈에서 include 하도록 변경

--- api/src/main/java/com/nb/api/ApiApplication.java
+++ api/src/main/java/com/nb/api/ApiApplication.java
@@ -2,7 +2,9 @@ package com.nb.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.nb"})
 @SpringBootApplication
 public class ApiApplication {
 
--- command/src/main/java/com/nb/command/subscriber/GroupJoinSubscriber.java
+++ command/src/main/java/com/nb/command/subscriber/GroupJoinSubscriber.java
@@ -3,6 +3,7 @@ package com.nb.command.subscriber;
 import com.nb.command.dto.ConnectionDto;
 import com.nb.command.service.ConnectionService;
 import com.nb.core.model.connection.Connection;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 import static com.nb.command.mapper.ConnectionMapper.toEntity;
 
+@Slf4j
 @Component
 public class GroupJoinSubscriber {
 
@@ -24,6 +26,7 @@ public class GroupJoinSubscriber {
 
     @KafkaListener(topics = "${kafka.topic}", containerFactory = "groupKafkaListenerContainerFactory")
     public void groupJoinListener(ConnectionDto dto) {
+        log.info("listener: {}", dto.toString());
         result = connectionService.saveConnection(toEntity(dto), LocalDateTime.now(Clock.systemDefaultZone()));
     }
 
